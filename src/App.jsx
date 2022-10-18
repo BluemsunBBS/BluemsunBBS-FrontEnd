c66cb10c826c11936996468700b728c8fb96bcde
yueyangw <yueyang_wu@outlook.com>
2022-10-18 15:11:13 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}>
-          <Route path="login" element={<LoginPage/>}></Route>
-          <Route path="home" element={<HomePage/>}></Route>
+        <Route index element={<HomePage/>} />
+        <Route path="/">
+          <Route path="login" element={<LoginPage/>} />
+          <Route path="home" element={<HomePage/>} />
         </Route>
         <Route path="*" element={<NotfoundPage/>} />
       </Routes>
