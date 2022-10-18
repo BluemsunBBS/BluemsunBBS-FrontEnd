93c1f4459f5a64aa037125eb5e22ab740e4d7555
Mengweiwei0123 <377595013@qq.com>
2022-10-18 14:02:24 +0800

    route

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -7,8 +7,8 @@ function Nav() {
         <div className="navContainer">
             <span className='bbsTitle'>BluemsunBBS</span>
             
-            <button className="navBtn" id="regis">创建新账户</button>
-            <button className="navBtn">登录</button>
+            <button className="navBtn" id="regis" onClick={()=>{window.location.href="/login"}}>创建新账户</button>
+            <button className="navBtn" onClick={()=>{window.location.href="/login"}}>登录</button>
             <span className="blockBox">
                 <img src={url} className="photo"></img>
                 <span className="hello">你好，陌生人</span>
