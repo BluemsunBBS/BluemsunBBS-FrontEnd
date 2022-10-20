bd8f2a5daac9c949714c65b8dcf60cf4a4c67595
yueyangw <yueyang_wu@outlook.com>
2022-10-20 20:34:20 +0800

    1

--- src/component/UserBlock/index.jsx
+++ src/component/UserBlock/index.jsx
@@ -1,35 +1,44 @@
+import { useEffect, useState } from 'react';
 import { http } from '../../utils/http';
 import url from './../../img/file.jpg'
 import './index.css'
 
 export default function UserBlock(props) {
 
-    var user = {
+    var [user, setUser] = useState({
         avatarUri: url,
         nickname: "用户",
         fans: 0
-    }
+    });
 
     const getUser = async (userId) => {
         if (userId == undefined) userId = null;
         if (userId == null) return;
         let res = await http.get(`/account/${userId}`);
-        if (res.status != 200 || res.code != 0) {
+        if (res.code != 0) {
             return;
         }
-        user.avatarUri = "http://bbs.wyy.ink:8080/images/" + res.data.avatar_uri;
-        user.nickname = res.data.nickname;
+        let avatarUri = "http://bbs.wyy.ink:8080/images/" + res.data.avatar_uri;
+        let nickname = res.data.nickname;
         res = await http.get(`/friend/countFans/${userId}`);
-        if (res.status != 200 || res.code != 0) {
+        if (res.code != 0) {
             return;
         }
-        user.fans = res.data;
+        setUser({
+            avatarUri: avatarUri,
+            nickname: nickname,
+            fans: res.data
+        });
     }
 
-    getUser(props.userId);
+    useEffect(()=>{getUser(props.userId)}, [])
+
+    const handleClick = () => {
+        window.location.href = `/user/${props.userId}`;
+    }
 
     return (
-        <div className='relatedMember'>
+        <div className='relatedMember' onClick={handleClick}>
             <img src={user.avatarUri} className="memberImg"></img>
             <span className='contentBox'>
                 <div className='text1'>{user.nickname}</div>
