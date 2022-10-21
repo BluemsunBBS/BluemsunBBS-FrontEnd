478db9782f7e53d8ab580bcedca0d0e784579b24
Mengweiwei0123 <377595013@qq.com>
2022-10-21 21:34:32 +0800

    1

--- src/component/NotificationContent/NotificationBlock/index.jsx
+++ src/component/NotificationContent/NotificationBlock/index.jsx
@@ -1,21 +1,24 @@
 import style from './index.module.css';
 
 export default function NotificationBlock(props) {
-
     const notification = props.notification;
+    var html = notification.text;
 
     console.log(notification);
 
     return (
-        <>
-            <div className={style.notificationBox}>
-                <div className={style.text1}>
-                    {notification.text}
-                </div>
-                <div className={style.text2}>
-                    {notification.notice_time}
-                </div>
+
+        <div className={style.notificationBox}>
+            <div className={style.text1} 
+                 dangerouslySetInnerHTML={{
+                    __html: html
+                 }}
+                 >
             </div>
-        </>
+            <div className={style.text2}>
+                {notification.notice_time}
+            </div>
+        </div>
+
     )
 }
