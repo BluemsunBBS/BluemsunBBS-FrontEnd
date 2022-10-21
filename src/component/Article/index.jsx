7d1b7301854697aed9c78d8c2963b08c763c714a
Mengweiwei0123 <377595013@qq.com>
2022-10-21 11:37:37 +0800

    css

---
+++ src/component/Article/index.jsx
@@ -0,0 +1,10 @@
+import style from './index.module.css'
+
+function Article(){
+    return(
+        <div className={style.articleBox}>
+            <span className={style.iconBox}></span>
+        </div>
+    )
+}
+export default Article;
