a5d17c739d9d491b673830f4ce36ed7452e9fcb6
Mengweiwei0123 <377595013@qq.com>
2022-10-26 21:07:58 +0800

    1

--- src/component/SelfCenter/EveryFollowBlock/index.jsx
+++ src/component/SelfCenter/EveryFollowBlock/index.jsx
@@ -2,10 +2,11 @@ import style from './index.module.css'
 
 export default function EveryFollowBlock(props) {
     const board = props.board;
-
+    var uri = "http://bbs.wyy.ink:8080/images/" + board.img;
     return (
 
-        <div className={style.notificationBox} >
+        <div className={style.msgBox} >
+            <img className={style.boardImg} src={uri}></img>
             <div className={style.text1}>{board.name}</div>
         </div>
 
