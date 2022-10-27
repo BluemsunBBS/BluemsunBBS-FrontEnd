9073ef04b915e264f55d3cd26d5e01609b0f3e14
Mengweiwei0123 <377595013@qq.com>
2022-10-28 00:01:02 +0800

    1

--- src/component/SelfCenter/EveryFollowPerson/index.jsx
+++ src/component/SelfCenter/EveryFollowPerson/index.jsx
@@ -1,6 +1,7 @@
 import style from './index.module.css'
 
 export default function EveryFollowPerson(props) {
+    console.log(props);
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.img;
     return (
