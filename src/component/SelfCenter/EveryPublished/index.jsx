c2a25d001219807f689ce4637db0d15ba4d16d2d
Mengweiwei0123 <377595013@qq.com>
2022-10-29 19:46:50 +0800

    1

--- src/component/SelfCenter/EveryPublished/index.jsx
+++ src/component/SelfCenter/EveryPublished/index.jsx
@@ -1,13 +1,18 @@
 import style from './index.module.css'
+import { useNavigate } from 'react-router';
 
 export default function EveryPublished(props) {
+    const navigate = useNavigate();
     console.log(props);
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.avatar_uri;
+    const handleClick = () =>{
+        navigate(`/article/${board.id}`);
+    }
     return (
 
         <div className={style.msgBox} >
-            <div className={style.text1}>{board.title}</div>
+            <div className={style.text1} onClick={handleClick}>{board.title}</div>
             <div className={style.text2}>{board.text.substr(0, 200)}</div>
             <span className={style.text3}>{board.create_time}</span>
         </div>
