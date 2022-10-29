8b59b3ad790e677a3afb903e9bb912853b741c45
Mengweiwei0123 <377595013@qq.com>
2022-10-29 21:34:49 +0800

    1

--- src/component/SelfCenter/Fans/index.jsx
+++ src/component/SelfCenter/Fans/index.jsx
@@ -70,7 +70,7 @@ export default function Fans() {
                         ))}
                     </div>
                 ) : (
-                    <NoMessage />
+                    <></>
                 )
             )}
             {data.page == 0 ? (<></>) : (
@@ -79,9 +79,10 @@ export default function Fans() {
                         <EveryFans key={board.id} board={board} />
                     ))
                 ) : (
-                    <NoMessage />
+                    <></>
                 )
             )}
+            {(data.total == 0 && friendData.total == 0)?(<NoMessage/>):(<></>)}
         </div>
     );
 }
