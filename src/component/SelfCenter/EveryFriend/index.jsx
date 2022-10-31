572c8460a0b3aad6101e0f0c8d552b00f90035ea
Mengweiwei0123 <377595013@qq.com>
2022-10-31 20:30:58 +0800

    1

--- src/component/SelfCenter/EveryFriend/index.jsx
+++ src/component/SelfCenter/EveryFriend/index.jsx
@@ -1,12 +1,17 @@
-import style from './index.module.css'
+import style from './index.module.css';
+import { useNavigate } from 'react-router';
 
 export default function EveryFriend(props) {
+    const navigate = useNavigate();
     console.log(props);
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.avatar_uri;
+    const handleClick = ()=>{
+        navigate(`/user/${board.id}`);
+    }
     return (
 
-        <div className={style.msgBox} >
+        <div className={style.msgBox} onClick={handleClick}>
             <img className={style.boardImg} src={uri}></img>
             <div className={style.text1}>{board.nickname}</div>
             <span className={style.text2}>已互关</span>
