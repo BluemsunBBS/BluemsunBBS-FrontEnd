38461a730b2811867c2353f455ba98f3d0050aef
Mengweiwei0123 <377595013@qq.com>
2022-11-01 16:35:52 +0800

    1

--- src/component/Admin/SideBar/index.jsx
+++ src/component/Admin/SideBar/index.jsx
@@ -68,7 +68,7 @@ export default function SideBar() {
             <p style={{marginTop: "20px", fontSize: "14px", color: "#fafafa"}}>欢迎您，{getUserInfo("nickname")}</p>
             <div className={style.navigationBar}>
                 {/* <p style={{textAlign: "left", fontSize: "13px", color: "#cacaca"}}>导航栏</p> */}
-                <Menu items={items} mode="inline" selectedKeys={[current]} onClick={(e)=>setCurrent(e.key)}/>
+                <Menu items={items} mode="inline" selectedKeys={[current]} onClick={(e)=>setCurrent(e.key)} className={style.menu}/>
             </div>
             <ManageContent />
         </div>
