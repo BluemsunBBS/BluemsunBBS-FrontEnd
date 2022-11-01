e73c2b6ec866609f6e088fd10640fc433676148a
Mengweiwei0123 <377595013@qq.com>
2022-11-01 11:48:14 +0800

    1

--- src/component/SelfCenter/EveryFans/index.jsx
+++ src/component/SelfCenter/EveryFans/index.jsx
@@ -2,8 +2,12 @@ import style from './index.module.css'
 import { Navigate, useNavigate } from 'react-router';
 import { http } from '../../../utils/http';
 import { message } from 'antd';
+import { useParams } from 'react-router';
+import { getUserInfo } from '../../../utils/func';
 
-export default function EveryFans(props) {
+export default function EveryFans (props) {
+    const useparams = useParams();
+    var logUserId = getUserInfo("id");
     const navigate = useNavigate();
     console.log(props);
     const board = props.board;
@@ -22,11 +26,11 @@ export default function EveryFans(props) {
     }
 
     return (
-
         <div className={style.msgBox}>
             <img className={style.boardImg} src={uri} onClick={handleClick}></img>
             <div className={style.text1}>{board.nickname}</div>
-            <button className={style.btn1} onClick={handleFollow}>关注</button>
+            {(useparams.id == logUserId.id)?(<button className={style.btn1} onClick={handleFollow}>关注</button>):(<></>)}
+            
         </div>
 
     )
--- src/component/SelfCenter/EveryFollowPerson/index.jsx
+++ src/component/SelfCenter/EveryFollowPerson/index.jsx
@@ -1,7 +1,13 @@
 import style from './index.module.css';
 import { useNavigate } from 'react-router';
+import { http } from '../../../utils/http';
+import { message } from 'antd';
+import { useParams } from 'react-router';
+import { getUserInfo } from '../../../utils/func';
 
 export default function EveryFollowPerson(props) {
+    const useparams = useParams();
+    var logUserId = getUserInfo("id");
     const navigate = useNavigate();
     console.log(props);
     const board = props.board;
@@ -14,6 +20,7 @@ export default function EveryFollowPerson(props) {
         <div className={style.msgBox} onClick={handleClick}>
             <img className={style.boardImg} src={uri}></img>
             <div className={style.text1}>{board.nickname}</div>
+            {(useparams.id == logUserId.id)?(<button className={style.btn1} onClick={handleFollow}>取消关注</button>):(<></>)}
         </div>
 
     )
