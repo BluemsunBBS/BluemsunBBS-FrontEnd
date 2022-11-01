097979dad285b6dce1e96aee8ef4c1a6fd79b1d8
Mengweiwei0123 <377595013@qq.com>
2022-11-01 11:17:01 +0800

    1

--- src/component/SelfCenter/EveryFans/index.jsx
+++ src/component/SelfCenter/EveryFans/index.jsx
@@ -1,20 +1,32 @@
 import style from './index.module.css'
 import { Navigate, useNavigate } from 'react-router';
+import { http } from '../../../utils/http';
+import { message } from 'antd';
 
 export default function EveryFans(props) {
     const navigate = useNavigate();
     console.log(props);
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.avatar_uri;
-    const handleClick = () =>{
+    const handleClick = () => {
         navigate(`user/${board.id}`);
     }
+
+    async function handleFollow() {
+        var res = await http.post(`/friend/${board.id}`);
+        if (res.code != 0) {
+            message.error(res.msg);
+        } else {
+            message.success("关注成功！");
+        }
+    }
+
     return (
 
-        <div className={style.msgBox} onClick={handleClick}>
-            <img className={style.boardImg} src={uri}></img>
+        <div className={style.msgBox}>
+            <img className={style.boardImg} src={uri} onClick={handleClick}></img>
             <div className={style.text1}>{board.nickname}</div>
-            <button className={style.btn1}>关注</button>
+            <button className={style.btn1} onClick={handleFollow}>关注</button>
         </div>
 
     )
