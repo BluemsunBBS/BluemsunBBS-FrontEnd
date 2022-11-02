2f79f273ad7e028883062491377934936eaef971
yueyangw <yueyang_wu@outlook.com>
2022-11-02 20:15:21 +0800

    1

--- src/component/Chat/index.jsx
+++ src/component/Chat/index.jsx
@@ -1,8 +1,33 @@
 import { MessageTwoTone } from "@ant-design/icons";
+import { message } from "antd";
+import { useEffect, useState } from "react";
+import { getUserInfo } from "../../utils/func";
+import { http } from "../../utils/http";
 import ChatDetail from "./ChatDetail";
 import style from "./index.module.css"
 
 export default function Chat() {
+
+    const [userList, setUserList] = useState([]);
+
+    useEffect(() => {
+        setUserList(fetchUserList());
+    }, []);
+
+    async function fetchUserList() {
+        let res = await http.get(`/friend/friendList/${getUserInfo("id")}`, {
+            params: {
+                page: 1,
+                size: 999
+            }
+        });
+        if (res.code != 0) {
+            message.error("获取用户列表失败");
+            return null;
+        }
+        return res.data.rows;
+    }
+
     return (
         <div className={style.chatBox}>
             <div className={style.userList}>
@@ -12,7 +37,11 @@ export default function Chat() {
                 }}>
                     <MessageTwoTone />&nbsp;&nbsp;个人私信
                 </span>
-
+                {userList.length != 0 ? (
+                    userList.map((user) => {
+                        <UserBlock key={user.id} user={user} />
+                    })
+                ) : (<></>)}
             </div>
             <div className={style.chatDetail}>
                 <ChatDetail />
