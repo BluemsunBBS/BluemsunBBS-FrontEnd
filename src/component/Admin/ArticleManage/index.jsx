277b925135c7e321b1cfc117001681675b6dc651
Mengweiwei0123 <377595013@qq.com>
2022-11-02 17:49:53 +0800

    1

--- src/component/Admin/ArticleManage/index.jsx
+++ src/component/Admin/ArticleManage/index.jsx
@@ -2,6 +2,11 @@ import style from './index.module.css';
 import NavOfAdmin from '../NavOfAdmin';
 import { Menu } from 'antd';
 import { useEffect, useState } from "react";
+import EveryBlock from './../EveryBlock';
+import NoMessage from './../NoMessage';
+import { message, notification} from 'antd';
+import { http } from '../../../utils/http';
+import { useNavigate } from 'react-router';
 
 const items = [
     {
@@ -14,6 +19,37 @@ const items = [
     }
 ]
 export default function ArticleManage() {
+    const [pager, setPager] = useState({
+        page: 1,
+        size: 10,
+    });
+
+    const APIResult = {
+        page: 0,
+        size: 0,
+        rows: [],
+        total: 0
+    }
+    const [data, setData] = useState(APIResult);
+    async function fetchList(pager) {
+        let res = await http.get(`/board/`,{
+            params: {
+                page: pager.page,
+                size: pager.size
+            }
+        });
+        if (res.code != 0) {
+            message.error(res.msg);
+            setData(APIResult);
+        } else {
+            setData(res.data);
+        }
+    }
+    useEffect(() => {
+        fetchList(pager);
+    }, [pager]);
+
+
     const [current, setCurrent] = useState('unchecked');
     const handleClick = (e) => {
         console.log('click ', e);
@@ -34,7 +70,15 @@ export default function ArticleManage() {
                 <div className={style.title}>管理文章</div>
                 <div className={style.text1}>请选择文章所属板块</div>
                 <div className={style.boardBox}>
-
+                    {data.page == 0 ? (<></>) : (
+                        (data && data.total != 0) ? (
+                            data.rows.map((board) => (
+                                <EveryBlock key={board.id} board={board} />
+                            ))
+                        ) : (
+                            <NoMessage />
+                        )
+                    )}
                 </div>
                 {/* <Menu
                     className={style.menu}
