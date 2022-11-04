84d3a75ab28eb38ec52e614a87c49547f5324047
Mengweiwei0123 <377595013@qq.com>
2022-11-04 21:35:55 +0800

    1

--- src/component/Admin/UserManage/index.jsx
+++ src/component/Admin/UserManage/index.jsx
@@ -1,12 +1,61 @@
 import style from './index.module.css';
 import NavOfAdmin from '../NavOfAdmin';
+import { useEffect, useState } from "react";
+import { message, notification } from 'antd';
+import { http } from '../../../utils/http';
+import { useNavigate } from 'react-router';
+import EveryFollowPerson from '../EveryFollowPerson';
 
 export default function UserManage() {
+    const [pager, setPager] = useState({
+        page: 1,
+        size: 10,
+    });
+
+    const APIResult = {
+        page: 0,
+        size: 0,
+        rows: [],
+        total: 0
+    }
+    const [data, setData] = useState(APIResult);
+    async function fetchList(pager) {
+        let res = await http.get(`/account/`, {
+            params: {
+                page: pager.page,
+                size: pager.size
+            }
+        });
+        if (res.code != 0) {
+            message.error(res.msg);
+            setData(APIResult);
+        } else {
+            setData(res.data);
+        }
+    }
+    useEffect(() => {
+        fetchList(pager);
+    }, [pager]);
+
     return (
         <div className={style.root}>
             <NavOfAdmin />
             <div className={style.contentBox}>
                 <div className={style.title}>管理用户</div>
+                <div className={style.btnBox}>
+                    <button className={style.btn1}>查看未封禁用户</button>
+                    <button className={style.btn1}>查看封禁用户</button>
+                </div>
+
+                {data.page == 0 ? (<></>) : (
+                    (data && data.total != 0) ? (
+                        data.rows.map((board) => (
+                            <EveryFollowPerson key={board.id} board={board} />
+                        ))
+                    ) : (
+                        <></>
+                    )
+                )}
             </div>
 
         </div>
