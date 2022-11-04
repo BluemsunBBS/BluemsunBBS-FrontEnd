e43cfb9a7a567907265260cb9a09e1a3ed65a176
Mengweiwei0123 <377595013@qq.com>
2022-11-04 16:51:55 +0800

    1

--- src/component/Chat/MessageBlock/index.jsx
+++ src/component/Chat/MessageBlock/index.jsx
@@ -10,11 +10,23 @@ export default function MessageBlock(props) {
 
     return (
         <div className={style.messageBlock}>
-            <Avatar src={`http://bbs.wyy.ink:8080/images/${avatarUrl}`} className={style.img}/>
-            <span className={style.nickName}>{msg.fromUser == user.id ? user.nickname : getUserInfo("nickname")}</span>
-            <div className={style.messageBubble}>
-                <p className={style.text}>{msg.text}</p>
-            </div>
+            {(msg.fromUser == user.id) ? (<div><Avatar src={`http://bbs.wyy.ink:8080/images/${avatarUrl}`} className={style.img} />
+                <div className={style.box}>
+                    <div className={style.nickName}>{msg.fromUser == user.id ? user.nickname : getUserInfo("nickname")}</div>
+                    <div className={style.messageBubble}>
+                        <p className={style.text}>{msg.text}</p>
+                    </div>
+                </div></div>
+
+
+            ) : (<div><Avatar src={`http://bbs.wyy.ink:8080/images/${avatarUrl}`} className={style.img1} />
+                <div className={style.box1}>
+                    <div className={style.nickName1}>{msg.fromUser == user.id ? user.nickname : getUserInfo("nickname")}</div>
+                    <div className={style.messageBubble}>
+                        <p className={style.text}>{msg.text}</p>
+                    </div>
+                </div></div>)}
         </div>
-    );
+
+    )
 }
