6a1a32218e9f8b0d45ab93dc52e9c970cb2a6ae8
Mengweiwei0123 <377595013@qq.com>
2022-11-04 20:48:18 +0800

    1

--- src/component/Chat/MessageBlock/index.jsx
+++ src/component/Chat/MessageBlock/index.jsx
@@ -20,15 +20,16 @@ export default function MessageBlock(props) {
 
 
             ) : (<div className={style.messageBlock1}>
-                <div className={style.imgBox}>
-                    <Avatar size={50} src={`http://bbs.wyy.ink:8080/images/${avatarUrl}`} />
-                </div>
                 <div className={style.box1}>
                     <div className={style.nickName1}>{msg.fromUser == user.id ? user.nickname : getUserInfo("nickname")}</div>
                     <div className={style.messageBubble}>
                         <p className={style.text1}>{msg.text}</p>
                     </div>
-                </div></div>)}
+                </div>
+                <div className={style.imgBox}>
+                    <Avatar size={50} src={`http://bbs.wyy.ink:8080/images/${avatarUrl}`} />
+                </div>
+                </div>)}
         </div>
 
     )
