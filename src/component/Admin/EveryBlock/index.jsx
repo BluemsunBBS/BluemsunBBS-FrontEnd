82dd1a43667d88f1b7a5dbe95bcb3f6952fab26d
Mengweiwei0123 <377595013@qq.com>
2022-11-06 01:02:45 +0800

    1

--- src/component/Admin/EveryBlock/index.jsx
+++ src/component/Admin/EveryBlock/index.jsx
@@ -1,16 +1,11 @@
 import style from './index.module.css';
-import { useNavigate } from 'react-router';
 
-const navigate = useNavigate();
 export default function EveryBlock(props) {
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.img;
-    const handleLink = (id)=>{
-        navigate(`/board/${id}`);
-      }
     return (
         <div className={style.msgBox} onClick={props.onClick}>
-            <img className={style.boardImg} src={uri} onClick={()=>{handleLink(board.id)}}></img>
+            <img className={style.boardImg} src={uri}></img>
             <div className={style.text1}>{board.name}</div>
             <span className={style.des}>{board.description}</span>
             <button className={style.btn1} onClick={()=>props.onDelete(board.id)}>删除版块</button>
--- src/component/Admin/EveryBlockOfBoard/index.jsx
+++ src/component/Admin/EveryBlockOfBoard/index.jsx
@@ -1,16 +1,11 @@
 import style from './index.module.css';
-import { useNavigate } from 'react-router';
 
 export default function EveryBlock(props) {
-    const Navigate = useNavigate();
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.img;
-    const handleLink = (id)=>{
-        Navigate(`/board/${id}`);
-      }
     return (
         <div className={style.msgBox} onClick={props.onClick}>
-            <img className={style.boardImg} src={uri} onClick={()=>{handleLink(board.id)}}></img>
+            <img className={style.boardImg} src={uri}></img>
             <div className={style.text1}>{board.name}</div>
             <span className={style.des}>{board.description}</span>
         </div>
