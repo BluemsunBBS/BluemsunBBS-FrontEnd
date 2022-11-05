710c2025e0a66d9d56d9f4606eb79454f135fe56
Mengweiwei0123 <377595013@qq.com>
2022-11-05 16:44:14 +0800

    1

--- src/component/Admin/BoardManage/index.jsx
+++ src/component/Admin/BoardManage/index.jsx
@@ -1,10 +1,18 @@
 import style from './index.module.css';
 import NavOfAdmin from '../NavOfAdmin';
 
-export default function ArticleManage(){
+export default function BoardManage(){
     return(
         <div className={style.root}>
-            <NavOfAdmin/>
+            <NavOfAdmin />
+            <div className={style.contentBox}>
+                <div className={style.title}>管理板块</div>
+                <div className={style.btnBox}>
+                    <button className={style.btn1}>查看未封禁用户</button>
+                    <button className={style.btn1}>查看封禁用户</button>
+                </div>
+            </div>
+
         </div>
         
     )
