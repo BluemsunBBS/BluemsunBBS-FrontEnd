f6a9cfe7e454a648e0a7f1ea53b99f6a102f33a7
Mengweiwei0123 <377595013@qq.com>
2022-11-06 01:13:51 +0800

    1

--- src/component/Admin/EveryBlock/index.jsx
+++ src/component/Admin/EveryBlock/index.jsx
@@ -3,9 +3,12 @@ import style from './index.module.css';
 export default function EveryBlock(props) {
     const board = props.board;
     var uri = "http://bbs.wyy.ink:8080/images/" + board.img;
+    const handleClick =()=>{
+        window.open(`/board/${board.id}`);
+    }
     return (
-        <div className={style.msgBox} onClick={props.onClick}>
-            <img className={style.boardImg} src={uri}></img>
+        <div className={style.msgBox}>
+            <img className={style.boardImg} src={uri} onClick={handleClick}></img>
             <div className={style.text1}>{board.name}</div>
             <span className={style.des}>{board.description}</span>
             <button className={style.btn1} onClick={()=>props.onDelete(board.id)}>删除版块</button>
