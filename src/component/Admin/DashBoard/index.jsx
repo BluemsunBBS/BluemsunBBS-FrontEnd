f9452518f7bd1022d6571d799f868d9dbebd24d6
Mengweiwei0123 <377595013@qq.com>
2022-11-06 14:18:42 +0800

    1

--- src/component/Admin/DashBoard/index.jsx
+++ src/component/Admin/DashBoard/index.jsx
@@ -1,3 +1,17 @@
+import NavOfAdmin from "../NavOfAdmin";
+import style from './index.module.css';
+import uri from './../../../img/bgc.jpg';
+
 export default function DashBoard() {
-    
+    return (
+        <div className={style.root}>
+            <NavOfAdmin />
+            <div className={style.imgBox}>
+                <div className={style.textBox}>
+                    <div>欢迎来到管理员界面</div>
+                </div>
+                <img src={uri} className={style.img} />
+            </div>
+        </div>
+    )
 }
